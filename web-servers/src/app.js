68c481bf8a6ca4daaab4d739fe9b58c308e1fa4f
manishm25 <manish.maurya@mail.vinove.com>
2019-03-15 11:15:49 +0530

    name changed

--- web-servers/src/app.js
+++ web-servers/src/app.js
@@ -18,21 +18,21 @@ hbs.registerPartials(partialspath);
 app.get('', (req, res) => {
   res.render('index', {
     title: 'Weather',
-    name: 'Andrew Mead'
+    name: 'Manish Maurya'
   });
 })
 
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About Page',
-    name: 'Andrew Mead'
+    name: 'Manish Maurya'
   });
 })
 
 app.get('/help', (req, res) => {
   res.render('help', {
     title: 'Help Page',
-    name: 'Andrew Mead'
+    name: 'Manish Maurya'
   });
 })
 
@@ -78,7 +78,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
   res.render('404', {
     title: '404',
-    name: 'Andrew Mead',
+    name: 'Manish Maurya',
     errorMessage: 'Help page article not found.'
   })
 })
@@ -86,7 +86,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
   res.render('404', {
     title: '404',
-    name: 'Andrew Mead',
+    name: 'Manish Maurya',
     errorMessage: 'Error: 404'
   })
 })
